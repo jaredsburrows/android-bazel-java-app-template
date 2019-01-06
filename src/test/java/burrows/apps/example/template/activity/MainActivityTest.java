27520839dfdfa23ad87a84d2959df11f00179ce1
Jingwen Chen <jin@crypt.sg>
2019-01-05 19:05:04 -0500

    RobolectricTestRunner -> AndroidJUnit4

--- src/test/java/burrows/apps/example/template/activity/MainActivityTest.java
+++ src/test/java/burrows/apps/example/template/activity/MainActivityTest.java
@@ -4,15 +4,16 @@ import android.app.Activity;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
-import org.robolectric.RobolectricTestRunner;
 import org.robolectric.android.controller.ActivityController;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Junit Test using Robolectric with AssertJ matchers.
  */
-@RunWith(RobolectricTestRunner.class)
+@RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
     @Test
     public void testOnCreateNotNull() {
